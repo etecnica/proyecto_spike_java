7128a788ae4720d5f162f0a80a6d0d51f2bdc518
etecnica <etecnica@gmail.com>
2026-05-05 13:56:03 -0500

    Configura Servlet Cliente en Tomcat

---
+++ backend_spike_java/ClienteServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ClienteServlet")
+public class ClienteServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("Servlet Cliente funcionando correctamente");
+    }
+}
